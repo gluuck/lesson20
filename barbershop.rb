7861cafc7917ae587d9962adfb3cc887500b23ad
Vladimir <ostapenkov74@gmail.com>
2020-02-25 12:37:53 +0500

    add post method handling

--- barbershop.rb
+++ barbershop.rb
@@ -3,3 +3,12 @@ require "sinatra/reloader"
 get '/' do
   erb :index
 end
+post '/' do
+  @name = params[:name]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+  #erb :index
+  @title = 'Thank you for rerite'
+  @message = "Dear #{@name} , your phone number #{@phone} , your date an time #{@date_time}"
+  erb :message
+end
