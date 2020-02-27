9b4cd251831c5d2ac20587e97f29568ba02f2bf8
Vladimir <ostapenkov74@gmail.com>
2020-02-27 13:01:05 +0500

    save to public

--- barbershop.rb
+++ barbershop.rb
@@ -3,11 +3,16 @@ require "sinatra/reloader"
 get '/' do
   erb :index
 end
-get '/admin' do
-  @text =File.open("user.txt","r")
-  erb :admin
-  #@text.close
-end
+# get '/admin' do
+#   if @admin =="admin" && @password =="123"
+#   @text =File.open("./public/user.txt","r")
+#   erb :admin
+#   else
+#   puts "not right password"
+#   #@text.close
+#   erb :admin
+#   end
+# end
 post '/' do
   @name = params[:name]
   @phone = params[:phone]
@@ -15,7 +20,7 @@ post '/' do
   #erb :index
   @title = 'Thank you for rerite'
   @message = "Dear #{@name} , your phone number #{@phone} , your date an time #{@date_time}"
-  f = File.open 'user.txt', 'a'
+  f = File.open './public/user.txt', 'a'
   f.write "User: #{@name}, Phone number: #{@phone} , date and time: #{@date_time}"
   f.close
   erb :message
